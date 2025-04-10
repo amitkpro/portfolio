7e43b269de4ce18864509e495fd675b2a3acb0d9
Amit kumar <asamitg971@gmail.com>
2025-04-10 09:36:14 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Hero from "./components/Hero"
 import About from "./components/About"
 import Skills from "./components/Skills"
 import Experience from "./components/Experience"
-// import Projects from "./components/Projects"
+// import Projects from "./components/Projects" 
 import Contact from "./components/Contact"
 import CursorTrailer from "./components/CursorTrailer"
 
